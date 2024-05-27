3ae8d68af86cf18d9c3613fd12834899e7a6ec6b
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-27 11:58:43 -0300

    projeto java

---
+++ GS-ECONOMIA-AZUL/src/main/java/br/com/fiap/beans/Ong.java
@@ -0,0 +1,30 @@
+package br.com.fiap.beans;
+
+public class Ong {
+	
+	private String nome;
+	private String areaAtuacao;
+	
+	public Ong(String nome, String areaAtuacao) {
+		super();
+		this.nome = nome;
+		this.areaAtuacao = areaAtuacao;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getAreaAtuacao() {
+		return areaAtuacao;
+	}
+	public void setAreaAtuacao(String areaAtuacao) {
+		this.areaAtuacao = areaAtuacao;
+	}
+	
+	
+	
+	
+}
